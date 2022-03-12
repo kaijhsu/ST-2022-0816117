171dffae59756e0597451e10e33fd4d6b1a9e865
kaijhsu <kaidz.cs08@nctu.edu.tw>
2022-03-12 23:35:22 +0800

    Update PriorityQueueTest.java

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,7 +14,7 @@ public class PriorityQueueTest {
 
     public static Stream<Arguments> streamProvider(){
         return Stream.of(
-                Arguments.of(new int[] {5, 4, 3}, new int[] {3, 4, 4}),
+                Arguments.of(new int[] {5, 4, 3}, new int[] {3, 4, 5}),
                 Arguments.of(new int[] {1, 4, 3}, new int[] {1, 3, 4}),
                 Arguments.of(new int[] {1}, new int[] {1}),
                 Arguments.of(new int[] {-1, 7, 2, -1}, new int[] {-1, -1, 2, 7}),
